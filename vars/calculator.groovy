1f2fb721ffbc7e94671dfbb9dc8435d96c08395e
sunilkumarkommu <48117909+sunilkumarkommu@users.noreply.github.com>
2021-12-09 20:09:05 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,6 +1,6 @@
 def add(x,y) {
-  echo "hey ${x} and ${x} is ${x+y}"
+  echo "hey ${x} and ${y} is ${x+y}"
 }
 def mul(x,y) {
-  echo "hey ${x} and ${x} is ${x*y}"
+  echo "hey ${x} and ${y} is ${x*y}"
 }
